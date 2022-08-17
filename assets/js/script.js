20f18d05f655884c090d5e55dee418ca2ee52e3a
BuildTools <unconfigured@null.spigotmc.org>
2022-08-16 21:56:47 -0600

    Exporting to Replit

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,3 +25,47 @@ document.addEventListener('DOMContentLoaded', function () {
         // TODO: Update the scoreboard (same logic as in flip button click handler)
 
 })
+let heads = 0
+let tails = 0
+let percentHeads = 0
+let percentTails = 0
+document.addEventListener('DOMContentLoaded', function(){
+    document.getElementById('flip').addEventListener('click', () => {
+        let headsFlip = Math.ceil (Math.random() < 0.5)
+        if (headsFlip) {
+            document.getElementById('penny-image').src = 'assets/images/penny-heads.jpg'
+            document.getElementById('message').textContent = 'You Flipped Heads!'
+            heads += 1
+        }
+        else {
+            document.getElementById('penny-image').src = 'assets/images/penny-tails.jpg'
+            document.getElementById('message').textContent = 'You Flipped Tails!'
+            tails += 1
+        }
+        let total = heads + tails
+        if (total > 0){
+            percentHeads = Math.round((headsRolls / total) * 100)
+            percentTails = Math.round((tailsRolls / total) * 100)
+        }
+        document.getElementById('heads').textContent = heads
+        document.getElementById('heads-percent').textContent = percentHeads + '%'
+        document.getElementById('tails').textContent = tails
+        document.getElementById('tails-percent').textContent = percentTails + '%'
+    })
+    document.getElementById('clear').addEventListener('click', function () {
+        heads = 0
+        tails = 0
+        document.getElementById('message').textContent = 'Let\'s Get Rolling!'
+        let total = heads + tails
+        let percentHeads = 0
+        let percentTails = 0
+        if (total > 0) {
+            percentHeads = Math.round((heads / total) * 100)
+            percentTails = Math.round((tails / total) * 100)
+        }
+        document.getElementById('heads').textContent = heads
+        document.getElementById('heads-percent').textContent = percentHeads + '%'
+        document.getElementById('tails').textContent = tails
+        document.getElementById('tails-percent').textContent = percentTails + '%'
+    })
+})
